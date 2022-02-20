e91b24a23d1a42a527d2943a8aad7c031ef1bdc0
vinaykottala <100072562+vinaykottala@users.noreply.github.com>
2022-02-20 22:51:31 +1100

    Update test.java

--- test.java
+++ test.java
@@ -14,3 +14,26 @@
 	 }
 	 return View(Message);
  }
+
+
+
+public boolean findEmailAddress( String emailAddress) throws Exception {
+
+Connection jdbcConnection = this.getConnection();
+	Statement sqlStatement = jdbcConnection.createStatement();
+	String sql = "SELECT email FROM user_details WHERE email = "" + emailAddress + "*";
+
+	ResultSet userSearch = sqlStatement.executeQuery(sql);
+
+	if (userSearch.first()) {
+		// We have a result, this user already exists; 
+	// We found this email address
+		return true;
+
+	}
+	else {
+	// We could not find an email address return false;
+		return false;
+	}
+}
+
